cc378d23de3c64e8a2ba6bd3e15eb9249fde1765
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 09:59:03 -0600

    Add state with empty reservations

--- src/App/App.js
+++ src/App/App.js
@@ -6,15 +6,20 @@ import reservations from '../sample-data/reservationData'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        
         </div>
         <div className='resy-container'>
-          
         </div>
       </div>
     )
