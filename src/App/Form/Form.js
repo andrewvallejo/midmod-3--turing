bfe788edc1009b182bee5a97437f92eab102865a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 10:43:34 -0600

    Add form skeleton

---
+++ src/App/Form/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Form.css'
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state= {
+      name: "",
+      date: "",
+      time: "",
+      number: "",
+    }
+  }
+  render() {
+    
+  }
+}
