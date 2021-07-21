d242e5815664e741f71a26ceea653706c418eb33
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 10:39:18 -0600

    Add card constructor

--- src/App/Card/Card.js
+++ src/App/Card/Card.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Card.css'
 
-const Card = () => {
-
+const Card = ({number, name, date, time, id}) => {
+  return (
+    <article className='card'>
+      <h4>{name}</h4>
+        <p>{date}</p>
+        <p>{time}</p>
+      <h3>Number of guests: {number}</h3>
+      <button>Cancel</button>
+    </article>
+  )
 }
 
 export default Card
