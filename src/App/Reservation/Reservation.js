7dccbc652efbdc3fe809b4f5cac1b8f132a84cbd
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 10:38:46 -0600

    Add all information needed to make an reservation

--- src/App/Reservation/Reservation.js
+++ src/App/Reservation/Reservation.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Reservation.css'
 
-const Reservation = () => {
-
+const Reservation = ({resyInfo}) => {
+  const reservationCards = resyInfo.map(res => {
+    return (
+      <Card
+        number={res.number}
+        name={res.name}
+        date={res.date}
+        time={res.time}
+        id={res.id}
+        key={res.id}
+      />
+    )
+  })
+  return (
+    <section className="resy-container">    
+    {reservationCards}
+    </section>
+  )
 }
 
 export default Reservation
