8aaf940a938d5e8305422949dfbc18530f97a118
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 12:12:36 -0600

    Add delete function to button

--- src/App/Card/Card.js
+++ src/App/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({number, name, date, time, id}) => {
+const Card = ({number, name, date, time, id, deleteResy}) => {
   return (
     <article className='card'>
       <h4>{name}</h4>
         <p>{date}</p>
         <p>{time}</p>
       <h3>Number of guests: {number}</h3>
-      <button>Cancel</button>
+      <button onClick={() => deleteResy(id)}>Cancel</button>
     </article>
   )
 }
