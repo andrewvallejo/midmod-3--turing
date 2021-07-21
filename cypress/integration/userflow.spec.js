1e62f84f67b25234a50dc10317b7ccad02117b2c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 11:43:12 -0600

    Add test to for upon visit

--- cypress/integration/userflow.spec.js
+++ cypress/integration/userflow.spec.js
@@ -2,4 +2,9 @@ describe('User Flows', () => {
   beforeEach(()=> {
     cy.visit('http://localhost:3000')
   })
+  it('Should show all reservations upon visit', () => {
+    cy.contains('Turing Cafe Reservations')
+    cy.get('.resy-container article').should('have.length', 9)
+  })
+
 })
