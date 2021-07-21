8990456d1d4d25783c3a145501af63aab808dcfe
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-21 11:16:50 -0600

    Add form functionality to make reservation

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -5,13 +5,63 @@ class Form extends Component {
   constructor() {
     super();
     this.state= {
+      id: 0,
       name: "",
       date: "",
       time: "",
-      number: "",
+      number: 0,
     }
   }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  submitReservation = (event) => {
+    event.preventDefault()
+    const newReservation = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addReservation(newReservation)
+  }
+
   render() {
-    
+    return ( 
+    <form>
+      <input 
+        type='text'
+        placeholder='Name'
+        name="name"
+        value={this.state.name}
+        onChange={event => this.handleChange(event)}
+      />
+            <input 
+        type='text'
+        placeholder='Date (mm/dd)'
+        name="date"
+        value={this.state.date}
+        onChange={event => this.handleChange(event)}
+      />
+            <input 
+        type='text'
+        placeholder='Time'
+        name="time"
+        value={this.state.time}
+        onChange={event => this.handleChange(event)}
+      />
+            <input 
+        type='number'
+        placeholder='Number of guests'
+        name="number"
+        value={this.state.number}
+        onChange={event => this.handleChange(event)}
+      />
+
+      <button onClick={event => this.submitReservation(event)}>Make Reservation</button>
+    </form>
+    )
   }
 }
+
+export default Form
